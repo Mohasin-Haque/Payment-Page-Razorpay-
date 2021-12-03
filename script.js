52eeba7f83df72ba8b358b4e42626a14c6e324b5
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-12-03 20:19:07 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector(".btn");
+const number = document.querySelector("#number-box");
+const outputDiv = document.querySelector(".output");
+
+// number.addEventListener("input", ()=>{
+//     if(number<=10){
+//         outputDiv.innerText = "Please enter a valid number";
+//       }else{
+//           outputDiv.innerText ="Please recheck the entered information";
+//       }
+// })
+
+btn.addEventListener("click" ()=>{
+    
+})
